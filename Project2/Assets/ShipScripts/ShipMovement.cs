34c2d6b4a51f374b66d82445d72484be87a8be4a
John Schlitters <jschlitters@gmail.com>
2019-09-07 00:33:59 -0600

    Tested some changes to shipmovement.cs
    Attempted to implement switch instead of if-then statement

--- Project2/Assets/ShipScripts/ShipMovement.cs
+++ Project2/Assets/ShipScripts/ShipMovement.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
-using UnityEngine.Events;
 using Vector3 = UnityEngine.Vector3;
 
 public class ShipMovement : MonoBehaviour
