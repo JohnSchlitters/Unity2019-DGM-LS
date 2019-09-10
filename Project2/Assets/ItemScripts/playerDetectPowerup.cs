157585b256f8798ab6ecaef353481992dc0987fc
John Schlitters <jschlitters@gmail.com>
2019-09-10 00:44:01 -0600

    2019-10-09
    - Added torpedo reload powerup
    - Fixed main gun reload powerup
    - Added sprite for torpedo reload
    - Began setup for ship HP pool

--- Project2/Assets/ItemScripts/playerDetectPowerup.cs
+++ Project2/Assets/ItemScripts/playerDetectPowerup.cs
@@ -8,6 +8,9 @@ public class playerDetectPowerup : MonoBehaviour
 
 {
     public MainGunTracking getReloadTime;
+
+    public TorpedoTubeTracking getTorpedoCount;
+
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D powerupCollision2D)
     {
@@ -17,17 +20,33 @@ public class playerDetectPowerup : MonoBehaviour
             print("starting boosted reload");
             Destroy(powerupCollision2D.gameObject);
             print("destroyed powerup");
+        }
 
-
+        if (powerupCollision2D.gameObject.name == "playerPowerUpTorpedo")
+        {
+            torpedoPowerUp();
+            print("starting torpedo boost");
+            Destroy(powerupCollision2D.gameObject);
+            print("destroyed powerup");
         }
     }
 
-    private IEnumerator reloadPowerUp()
-    { 
-        gameObject.GetComponent<MainGunTracking>().playerReloadTime = 2.0f;
+    public IEnumerator reloadPowerUp()
+    {
+        getReloadTime.playerArtilleryReload = false;
+        getReloadTime.playerReloadTime = 2;
         print("set reload to 2s");
         yield return new WaitForSeconds(15);
-        gameObject.GetComponent<MainGunTracking>().playerReloadTime = 4.0f;
+        getReloadTime.playerReloadTime = 4;
         print("player reload returned to normal");
     }
+
+    public void torpedoPowerUp()
+    {
+        StopCoroutine(getTorpedoCount.playerTorpedoReloadFunc());
+        getTorpedoCount.playerTorpedoCount = 4;
+        getTorpedoCount.uiShipTorpedoStatus.text = ("Torpedo Tubes " + getTorpedoCount.playerTorpedoCount + " Ready");
+        print("fully reloaded torpedo tube");
+    }
+
 }
---
+++ Project2/Assets/ShipScripts/ShipIntegrity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipIntegrity : MonoBehaviour
+{
+    public int playerShipIntegrity = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("starting ship HP tracking");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerShipIntegrity > 0)
+        {
+            
+        }
+        else
+        {
+  //          Destroy(gameObject.name == "player_ShipLaforey");
+        }
+    }
+}
