e19f6574f65ae7b0a32f617e4565e39078cf2b9e
John Schlitters <jschlitters@gmail.com>
2019-10-01 15:37:56 -0600

    2019-01-10
    -Added framework for enemy planes
    -Added framework for player AA
    -Added framework for enemy plane attacks
    -Added framework for enemy HP

---
+++ Project2/Assets/Enemy Scripts/EnemyPlaneAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlaneAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Started an enemy plane AI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
