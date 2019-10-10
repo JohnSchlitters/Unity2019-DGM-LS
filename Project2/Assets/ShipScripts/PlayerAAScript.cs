1f8c53296f1742d24527bda9d4964080b669f235
John Schlitters <jschlitters@gmail.com>
2019-10-09 19:39:08 -0600

    2019-08-10
    Started setup for AA scripts

--- Project2/Assets/ShipScripts/PlayerAAScript.cs
+++ Project2/Assets/ShipScripts/PlayerAAScript.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerAAScript : MonoBehaviour
 {
+    private float playerAACycleTime = 1.5f;
+    private float playerAAMaxDistance = 25f;
+    public float enemyTargetDistance;
+    public Transform enemyTarget;
+
+
+    [SerializeField] public Transform aaGunBarrel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //public transform enemyTarget = GameObject.FindWithTag("EnemyPlane");
+       //  enemyTarget = gameObject();
+        enemyTargetDistance = Vector3.Distance(enemyTarget.transform.position, aaGunBarrel.transform.position);
+
+        if (enemyTargetDistance < playerAAMaxDistance)
+        {
+            TrackTarget();
+        }
+    }
+
+    private void TrackTarget()
+    {
+
     }
 }
+    
+    
